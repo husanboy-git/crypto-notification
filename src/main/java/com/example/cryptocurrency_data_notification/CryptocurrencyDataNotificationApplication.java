887780fa7b774a1f3665c4ff89506f48c289303b
Huanboy <hus19950319@gmail.com>
2025-04-21 18:40:19 +0900

    refactoring commit

--- src/main/java/com/example/cryptocurrency_data_notification/CryptocurrencyDataNotificationApplication.java
+++ src/main/java/com/example/cryptocurrency_data_notification/CryptocurrencyDataNotificationApplication.java
@@ -2,12 +2,14 @@ package com.example.cryptocurrency_data_notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CryptocurrencyDataNotificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptocurrencyDataNotificationApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CryptocurrencyDataNotificationApplication.class, args);
+		SpringApplication.exit(run);
 	}
 
 }
